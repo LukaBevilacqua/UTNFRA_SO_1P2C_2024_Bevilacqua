9679b69e628c74fce4308c3086ce525cb27a5a36
LukaBevilacqua <lukabevilacqua@gmail.com>
2024-10-12 05:35:06 +0000

    add: punto D resuelto

---
+++ RTA_SCRIPT_Examen_20241012/Punto_D.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p Estructura_Asimetrica/correo Estructura_Asimetrica/clientes
+touch Estructura_Asimetrica/correo/cartas{1..100}
+touch Estructura_Asimetrica/clientes/cartas{1..100}
+touch Estructura_Asimetrica/correo/carteros{1..10}
