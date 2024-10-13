6957ede8872b95c222757b65c2c3261cb7b6aae2
LukaBevilacqua <lukabevilacqua11@gmail.com>
2024-10-13 21:45:43 +0000

    add: punto F terminado

--- RTA_SCRIPT_Examen_20241012/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241012/Punto_E.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat /proc/meminfo | grep -i "MemTotal" > RTA_ARCHIVOS_Examen20241013/Filtro_Basico.txt
-sudo dmidecode -t chassis | grep -i "Manufacturer" >> Filtro_Basico.txt
+cat /proc/meminfo | grep -i "MemTotal" > ../RTA_ARCHIVOS_Examen_20241013/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep -i "Manufacturer" >> ../RTA_ARCHIVOS_Examen_20241013/Filtro_Basico.txt
